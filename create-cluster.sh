3e02d9d1878ac92288228d6b2a23668d9d711bc0
blackmoonhp95 <blackmoon8668@gmail.com>
2022-04-30 17:27:26 +0700

    Modify create-cluster.sh

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Variables
-resourceGroup="acdnd-c4-project"
-clusterName="udacity-cluster"
+resourceGroup="echancing_applications"
+clusterName="quangnm-cluster"
 
 # Install aks cli
 echo "Installing AKS CLI"
@@ -23,19 +23,6 @@ az aks create \
 --enable-addons monitoring \
 --generate-ssh-keys
 
-# For Cloud Lab users
-az aks create \
---resource-group $resourceGroup \
---name $clusterName \
---node-count 1 \
---generate-ssh-keys
-
-# For Cloud Lab users
-# This command will is a substitute for "--enable-addons monitoring" option in the "az aks create"
-# Use the log analytics workspace - Resource ID
-# For Cloud Lab users, go to the existing Log Analytics workspace --> Properties --> Resource ID. Copy it and use in the command below.
-az aks enable-addons -a monitoring -n $clusterName -g $resourceGroup --workspace-resource-id "/subscriptions/6c39f60b-2bb1-4e37-ad64-faaf30beaca4/resourcegroups/cloud-demo-153430/providers/microsoft.operationalinsights/workspaces/loganalytics-153430"
-
 echo "AKS cluster created: $clusterName"
 
 # Connect to AKS cluster
@@ -51,6 +38,6 @@ echo "Verifying connection to $clusterName"
 
 kubectl get nodes
 
-# echo "Deploying to AKS cluster"
+ echo "Deploying to AKS cluster"
 # The command below will deploy a standard application to your AKS cluster. 
-# kubectl apply -f azure-vote.yaml
+kubectl apply -f azure-vote.yaml
